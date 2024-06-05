9d8aebc672b5df9304db86274abcd07ce6b5e4f4
vbnm134678 <syj1351@naver.com>
2024-06-05 18:08:13 +0900

    todo 테이블 할일 추가 input 버튼, 텍스트 버튼 추가

--- components/todos-table.tsx
+++ components/todos-table.tsx
@@ -1,10 +1,14 @@
 "use client"
 
-import React from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Input, Button, PopoverTrigger, PopoverContent, Popover } from "@nextui-org/react";
 import { Todo } from "@/types"
 
 const TodosTable = ({ todos }: { todos: Todo[] }) => {
+  const [todoAddEnable, setTodoAddEnable] = useState(false);
+  const [newTodoInput, setNewTodoInput] = useState("");
+
+  // 할일 list row
   const TodoRow = (aTodo: Todo) => {
     return <TableRow key={aTodo.id}>
       <TableCell>{aTodo.id.slice(0, 5)}</TableCell>
@@ -13,20 +17,49 @@ const TodosTable = ({ todos }: { todos: Todo[] }) => {
       <TableCell>{`${aTodo.created_at}`}</TableCell>
     </TableRow>
   }
+  // 할일 추가버튼 popover
+  const disableTodoAddButton = () => {
+    return <Popover placement="top" showArrow={true}>
+      <PopoverTrigger>
+        <Button disabled className="h-14" color="default">할일 추가</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="px-1 py-2">
+          <div className="text-small font-bold">할일을 입력해주세요</div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  }
+
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>ID</TableColumn>
-        <TableColumn>할일</TableColumn>
-        <TableColumn>완료여부</TableColumn>
-        <TableColumn>생성일</TableColumn>
-      </TableHeader>
-      <TableBody emptyContent={"No data to show"}>
-        {todos && todos.map((aTodo: Todo) => (
-          TodoRow(aTodo)
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input className="h-14" type="text" label="새로운 할일"
+          value={newTodoInput}
+          onValueChange={(changedInput) => {
+            setNewTodoInput(changedInput);
+            setTodoAddEnable(changedInput.length > 0)
+          }} />
+        {todoAddEnable
+          ? <Button className="h-14" color="warning">할일 추가</Button>
+          : disableTodoAddButton()
+
+        }      </div>
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>할일</TableColumn>
+          <TableColumn>완료여부</TableColumn>
+          <TableColumn>생성일</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No data to show"}>
+          {todos && todos.map((aTodo: Todo) => (
+            TodoRow(aTodo)
+          ))}
+        </TableBody>
+      </Table>
+    </>
+
   );
 }
 
